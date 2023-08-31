63b5100f713f6b71ea533929aa2cd1b934e1f9a6
staticWagomU <to6wa17@gmail.com>
2023-08-31 18:14:32 +0900

    プロジェクト ファイルを追加します。

---
+++ etude-aspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace etude_aspNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
