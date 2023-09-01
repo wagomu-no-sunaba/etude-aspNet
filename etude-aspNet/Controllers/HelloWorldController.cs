3eae5c3ca21fd79c78ea2d38558c54d96e18ef1f
staticWagomU <to6wa17@gmail.com>
2023-09-01 10:40:05 +0900

    APIのエンドポイントを追加

--- etude-aspNet/Controllers/HelloWorldController.cs
+++ etude-aspNet/Controllers/HelloWorldController.cs
@@ -4,9 +4,18 @@ namespace etude_aspNet.Controllers
 {
     public class HelloWorldController : Controller
     {
-        public IActionResult Index()
+        //
+        // GET: /HelloWorld/
+        public string Index()
         {
-            return View();
+            return "This is my default action...";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
         }
     }
 }
